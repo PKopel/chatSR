24eca1f676bf6213d4d2567fbdbf3417253ac9c7
PKopel <pawel.kopel2@gmail.com>
2021-03-13 16:48:40 +0100

    improve client ui with crossterm

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,22 +1,35 @@
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent},
+    execute,
+    style::Print,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use json;
 use std::io::{self, Error, Read, Write};
-use std::net::TcpStream;
 use std::net::UdpSocket;
+use std::net::{Shutdown, TcpStream};
 use std::str;
-use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
 struct Client {
     nick: String,
-    stream: Arc<Mutex<TcpStream>>,
-    socket: Arc<UdpSocket>,
+    stream: TcpStream,
+    socket: UdpSocket,
 }
+
+const MENU: &str = r#"TCP/UDP chat
+Controls:
+ - 'q' - quit
+ - 't' - send TCP message
+ - 'u' - send UDP message
+"#;
 
 fn get_nick() -> String {
     print!("your nick: ");
     let mut nick = String::new();
     io::stdout().flush().unwrap();
     match io::stdin().read_line(&mut nick) {
-        Ok(_) => return nick,
+        Ok(_) => return String::from(nick.trim_end()),
         _ => {
             println!("try again");
             return get_nick();
@@ -25,33 +38,37 @@ fn get_nick() -> String {
 }
 
 fn start_client() -> Result<(Client, JoinHandle<()>, JoinHandle<()>), Error> {
-    println!("starting client...");
-    let stream = Arc::new(Mutex::new(TcpStream::connect("127.0.0.1:34254")?));
-    let socket = Arc::new(UdpSocket::bind("127.0.0.1:34254")?);
+    println!("starting client...\n{}", MENU);
+    let stream = TcpStream::connect("127.0.0.1:34254")?;
+    let socket = UdpSocket::bind("127.0.0.1:34255")?;
     let nick = get_nick();
     let client = Client {
         nick: nick,
-        stream: stream.clone(),
-        socket: socket.clone(),
+        stream: stream.try_clone()?,
+        socket: socket.try_clone()?,
     };
     let tcp_handle = start_tcp_receiver(stream);
     let udp_handle = start_udp_receiver(socket);
     Ok((client, tcp_handle, udp_handle))
 }
 
-fn show_msg(buff: [u8; 1024]) {
-    match str::from_utf8(&buff) {
-        Ok(text) => println!("{}", text),
+fn show_msg(buff: &[u8], size: usize) {
+    match str::from_utf8(&buff[..size]) {
+        Ok(msg) => {
+            let msg_obj = json::parse(msg).unwrap();
+            print!("\r{}> {}", msg_obj["nick"], msg_obj["text"]);
+            io::stdout().flush().unwrap()
+        }
         Err(err) => println!("{}", err),
     }
 }
 
-fn start_tcp_receiver(stream: Arc<Mutex<TcpStream>>) -> JoinHandle<()> {
+fn start_tcp_receiver(mut stream: TcpStream) -> JoinHandle<()> {
     return thread::spawn(move || {
         let mut msg_buff = [0 as u8; 1024];
         loop {
-            match stream.lock().unwrap().read(&mut msg_buff) {
-                Ok(_) => show_msg(msg_buff),
+            match stream.read(&mut msg_buff) {
+                Ok(size) => show_msg(&msg_buff, size),
                 Err(err) => {
                     println!("{}", err);
                     break;
@@ -61,12 +78,12 @@ fn start_tcp_receiver(stream: Arc<Mutex<TcpStream>>) -> JoinHandle<()> {
     });
 }
 
-fn start_udp_receiver(socket: Arc<UdpSocket>) -> JoinHandle<()> {
+fn start_udp_receiver(socket: UdpSocket) -> JoinHandle<()> {
     return thread::spawn(move || {
         let mut msg_buff = [0 as u8; 1024];
         loop {
             match socket.recv_from(&mut msg_buff) {
-                Ok(_) => show_msg(msg_buff),
+                Ok((size, _addr)) => show_msg(&msg_buff, size),
                 Err(err) => {
                     println!("{}", err);
                     break;
@@ -76,19 +93,60 @@ fn start_udp_receiver(socket: Arc<UdpSocket>) -> JoinHandle<()> {
     });
 }
 
-fn send_tcp(msg_text: &str, client: Client) -> std::io::Result<()> {
-    Ok(())
+fn prepare_msg(nick: &str) -> String {
+    execute!(io::stdout(), EnterAlternateScreen, Print("your message: ")).unwrap();
+    let mut msg_text = String::new();
+    io::stdin().read_line(&mut msg_text).unwrap();
+    execute!(io::stdout(), LeaveAlternateScreen).unwrap();
+    json::stringify(json::object! {
+        nick: nick,
+        text: msg_text
+    })
 }
 
-fn send_udp(msg_text: &str, client: Client) -> std::io::Result<()> {
-    Ok(())
+fn read_char() -> std::io::Result<char> {
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            ..
+        })) = event::read()
+        {
+            return Ok(c);
+        }
+    }
+}
+
+fn user_loop(mut client: Client) -> crossterm::Result<()> {
+    let nick = client.nick;
+    loop {
+        terminal::enable_raw_mode()?;
+        match read_char()? {
+            'u' => {
+                terminal::disable_raw_mode()?;
+                let msg = prepare_msg(&nick);
+                client.socket.send_to(msg.as_bytes(), "127.0.0.1:34254")?;
+            }
+            't' => {
+                terminal::disable_raw_mode()?;
+                let msg = prepare_msg(&nick);
+                client.stream.write(msg.as_bytes())?;
+            }
+            'q' => break,
+            _ => {}
+        };
+    }
+    client.stream.shutdown(Shutdown::Both)?;
+    println!("bye!");
+    terminal::disable_raw_mode()
 }
 
 fn main() -> std::io::Result<()> {
-    let client = match start_client() {
+    let (client, tcp_handle, udp_handle) = match start_client() {
         Ok(client) => client,
         Err(err) => return Err(err),
     };
-    println!("bye!");
+    user_loop(client).unwrap();
+    tcp_handle.join().unwrap();
+    udp_handle.join().unwrap();
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,45 @@
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 1024]; // using 50 byte buffer
+    loop {
+        match stream.read(&mut data) {
+            Ok(size) => {
+                stream.write(&data[0..size]).unwrap();
+            }
+            Err(_) => {
+                println!(
+                    "An error occurred, terminating connection with {}",
+                    stream.peer_addr().unwrap()
+                );
+                stream.shutdown(Shutdown::Both).unwrap();
+                break;
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("server")
+    let listener = TcpListener::bind("127.0.0.1:34254").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on port 34254");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
 }
